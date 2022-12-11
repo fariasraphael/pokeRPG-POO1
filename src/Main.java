f9dc880cbb1932bc16e5625a87d4bf8d2448e184
João Gomes <joao.gomes@ddmx.com.br>
2022-12-11 10:05:41 -0300

    Utilização da classe statica para criar os elementos do jogo e criação de um novo bulbassaur

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import classes.pokemons.Bulbassaur;
+import classes.sistema.Elemento;
+import classes.sistema.Statics;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        inicializaElementos();
+
+        new Bulbassaur("Jefferson");
+    }
+
+    private static void inicializaElementos() {
+        // inicializando os 3 elementos basicos FOGO, AGUA, GRAMA
+        Elemento fogo = new Elemento("fogo");
+        Elemento agua = new Elemento("agua");
+        Elemento grama = new Elemento("grama");
+
+        // configurando vantagens e fraquezas
+        fogo.setFraqueza(agua);
+        fogo.setVantagem(grama);
+
+        agua.setFraqueza(grama);
+        agua.setVantagem(fogo);
+
+        grama.setFraqueza(fogo);
+        grama.setVantagem(agua);
+
+        // salvando elementos no statics
+        Statics.setELEMENTO("fogo", fogo);
+        Statics.setELEMENTO("agua", agua);
+        Statics.setELEMENTO("grama", grama);
     }
 }

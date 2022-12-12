d8472ed199b7e362160edc73f93ace3851ecad11
jackson-veloso <jackson.veloso@hotmail.com>
2022-12-12 20:27:45 -0300

    add upgrade aos golpes class Charmeleon

--- src/classes/pokemons/Charmeleon.java
+++ src/classes/pokemons/Charmeleon.java
@@ -2,7 +2,9 @@ package classes.pokemons;
 
 import classes.abstracts.Pokemon;
 import classes.sistema.Golpe;
+import classes.sistema.Golpes;
 import classes.sistema.Statics;
+import classes.sistema.StaticsGolpe;
 
 import java.util.Random;
 
@@ -11,11 +13,25 @@ public class Charmeleon extends Charmander {
     public Charmeleon(String nome) {
         super(nome);
         super.setVida(super.getVida() + (new Random().nextInt(200) + 100));
+        super.setGolpes(upgradeGolpes());
         System.out.printf("Criando um novo %s\n", this);
     }
 
-    @Override
-    public int ataca(Golpe golpe) {
-        return 0;
+    private Golpe[] upgradeGolpes(){
+        Golpe[] golpes = {
+                StaticsGolpe.getGOLPE("growl"),
+                StaticsGolpe.getGOLPE("fireFang"),
+                StaticsGolpe.getGOLPE("ember"),
+                StaticsGolpe.getGOLPE("flameThrower"),
+        };
+
+        golpes[0].setDano(100 + new Random().nextInt(70));
+        golpes[1].setDano(150 + new Random().nextInt(70));
+        golpes[2].setDano(200 + new Random().nextInt(70));
+        golpes[3].setDano(300 + new Random().nextInt(70));
+
+        return golpes;
     }
+
+
 }

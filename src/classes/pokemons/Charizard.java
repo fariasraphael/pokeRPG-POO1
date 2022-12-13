9bdbe545e027f541738116c941d3c89cd0e94935
jackson-veloso <jackson.veloso@hotmail.com>
2022-12-13 18:56:30 -0300

    alterado o construtor para não mais setar o atributo vida pois receberá a vida ao ser evoluido pelo charmeleon. Criado o metodo evolui() que retorna o proprio objeto charizard

--- src/classes/pokemons/Charizard.java
+++ src/classes/pokemons/Charizard.java
@@ -9,12 +9,17 @@ public class Charizard extends Charmeleon {
 
     public Charizard(String nome) {
         super(nome);
-        super.setVida(super.getVida() + (new Random().nextInt(300) + 200));
+        //super.setVida(super.getVidaMaxima() + 200);//(new Random().nextInt(300) + 200));
         System.out.printf("Criando um novo %s\n", this);
     }
 
     @Override
     public int ataca(Golpe golpe, Pokemon pokemonInimigo) {
         return 0;
+    }
+
+    @Override
+    public Charizard evolui() {
+        return this;
     }
 }

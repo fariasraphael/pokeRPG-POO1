0ed67c60bee23b6b4cfb4ac4e130d65f5d960d5d
jackson-veloso <jackson.veloso@hotmail.com>
2022-12-13 20:59:30 -0300

    criado 04 golpes do tipo grama e alterado o o maximo de usos para ser randomico

--- src/classes/sistema/Golpes.java
+++ src/classes/sistema/Golpes.java
@@ -14,33 +14,67 @@ public class Golpes {
         growl.setNome("growl");
         growl.setDano(50 + new Random().nextInt(50));
         growl.setTipo(Statics.getELEMENTO("fogo"));
-        growl.setUsos(5);
-        growl.setMaxUsos(5);
+        growl.setMaxUsos(2 + new Random().nextInt(4));
+        growl.setUsos(growl.getMaxUsos());
 
         fireFang.setNome("fireFang");
         fireFang.setDano(50 + new Random().nextInt(100));
         fireFang.setTipo(Statics.getELEMENTO("fogo"));
-        fireFang.setUsos(4);
-        fireFang.setMaxUsos(4);
+        fireFang.setMaxUsos(2 + new Random().nextInt(3));
+        fireFang.setUsos(fireFang.getMaxUsos());
 
         ember.setNome("ember");
         ember.setDano(100 + new Random().nextInt(100));
         ember.setTipo(Statics.getELEMENTO("fogo"));
-        ember.setUsos(3);
-        ember.setMaxUsos(3);
+        ember.setMaxUsos(1 + new Random().nextInt(4));
+        ember.setUsos(ember.getMaxUsos());
 
         flameThrower.setNome("flameThrower");
         flameThrower.setDano(150 + new Random().nextInt(150));
         flameThrower.setTipo(Statics.getELEMENTO("fogo"));
-        flameThrower.setUsos(2);
-        flameThrower.setMaxUsos(2);
-
-
+        flameThrower.setMaxUsos(1 + new Random().nextInt(2));
+        flameThrower.setUsos(flameThrower.getMaxUsos());
 
         StaticsGolpe.setGOLPE("growl", growl);
         StaticsGolpe.setGOLPE("fireFang", fireFang);
         StaticsGolpe.setGOLPE("ember", ember);
         StaticsGolpe.setGOLPE("flameThrower", flameThrower);
+
+
+        //golpes pokemon do tipo Grama
+        Golpe razorLeaf = new Golpe();
+        Golpe vineWhip = new Golpe();
+        Golpe whirlwind = new Golpe();
+        Golpe petalDance = new Golpe();
+
+        razorLeaf.setNome("razorLeaf");
+        razorLeaf.setDano(50 + new Random().nextInt(50));
+        razorLeaf.setTipo(Statics.getELEMENTO("grama"));
+        razorLeaf.setMaxUsos(2 + new Random().nextInt(4));
+        razorLeaf.setUsos(razorLeaf.getMaxUsos());
+
+        vineWhip.setNome("vineWhip");
+        vineWhip.setDano(50 + new Random().nextInt(100));
+        vineWhip.setTipo(Statics.getELEMENTO("grama"));
+        vineWhip.setMaxUsos(2 + new Random().nextInt(3));
+        vineWhip.setUsos(vineWhip.getMaxUsos());
+
+        whirlwind.setNome("whirlwind");
+        whirlwind.setDano(100 + new Random().nextInt(100));
+        whirlwind.setTipo(Statics.getELEMENTO("grama"));
+        whirlwind.setMaxUsos(1 + new Random().nextInt(4));
+        whirlwind.setUsos(whirlwind.getMaxUsos());
+
+        petalDance.setNome("petalDance");
+        petalDance.setDano(150 + new Random().nextInt(150));
+        petalDance.setTipo(Statics.getELEMENTO("grama"));
+        petalDance.setMaxUsos(1 + new Random().nextInt(2));
+        petalDance.setUsos(petalDance.getMaxUsos());
+
+        StaticsGolpe.setGOLPE("razorLeaf", razorLeaf);
+        StaticsGolpe.setGOLPE("vineWhip", vineWhip);
+        StaticsGolpe.setGOLPE("whirlwind", whirlwind);
+        StaticsGolpe.setGOLPE("petalDance", petalDance);
 
     }
 }

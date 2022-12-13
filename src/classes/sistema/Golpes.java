1802d07c35c71f0126ec68575befbf2031a38d29
jackson-veloso <jackson.veloso@hotmail.com>
2022-12-13 19:25:59 -0300

    alterado os atributos setUsos e setMaxUsos dos golpes

--- src/classes/sistema/Golpes.java
+++ src/classes/sistema/Golpes.java
@@ -14,26 +14,26 @@ public class Golpes {
         growl.setNome("growl");
         growl.setDano(50 + new Random().nextInt(50));
         growl.setTipo(Statics.getELEMENTO("fogo"));
-        growl.setUsos(0);
-        growl.setMaxUsos(3);
+        growl.setUsos(5);
+        growl.setMaxUsos(5);
 
         fireFang.setNome("fireFang");
         fireFang.setDano(50 + new Random().nextInt(100));
         fireFang.setTipo(Statics.getELEMENTO("fogo"));
-        fireFang.setUsos(0);
-        fireFang.setMaxUsos(3);
+        fireFang.setUsos(4);
+        fireFang.setMaxUsos(4);
 
         ember.setNome("ember");
         ember.setDano(100 + new Random().nextInt(100));
         ember.setTipo(Statics.getELEMENTO("fogo"));
-        ember.setUsos(0);
+        ember.setUsos(3);
         ember.setMaxUsos(3);
 
         flameThrower.setNome("flameThrower");
         flameThrower.setDano(150 + new Random().nextInt(150));
         flameThrower.setTipo(Statics.getELEMENTO("fogo"));
-        flameThrower.setUsos(0);
-        flameThrower.setMaxUsos(3);
+        flameThrower.setUsos(2);
+        flameThrower.setMaxUsos(2);
 
 
 

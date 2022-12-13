bf1f09671cc4f6801e85e1b96f64be1db6fa95f5
João Gomes <joao.gomes@ddmx.com.br>
2022-12-12 23:01:11 -0300

    Correção na classe Revive

--- src/classes/Itens/Revive.java
+++ src/classes/Itens/Revive.java
@@ -6,12 +6,15 @@ import classes.abstracts.Pokemon;
 public class Revive extends Item {
 
     @Override
-    public void usarItem (Pokemon pokemon){
+    public Boolean usarItem (Pokemon pokemon){
         if(!pokemon.isReviveu()){
             //pokemon.setVida(pokemon.getVidaInicial() * 0.75); // se considerar que a vida de cada pokemon pode variar para aumentar a dificuldade da batalha
-            pokemon.setVida(750); //Se considerarmos que na criacao todos pokemons tem vida igual a 1000
+            pokemon.setVida((int) (0.75 * pokemon.getVidaMaxima())); //Se considerarmos que na criacao todos pokemons tem vida igual a 1000
             pokemon.setReviveu(true);
+
+            return true;
         }
+        return false;
     }
 
 }
